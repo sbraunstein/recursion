a94e6367b8cdf91faf940a1a7f32396200868e9c
sbraunstein <sbraunstein12@gmail.com>
2015-04-19 21:04:08 -0500

    15 0f 20 fixtures passing. Still need objects with null and nested objects to pass.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,61 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
-};
+
+function stringifyJSON(obj){
+		if(obj === null){
+			return "null";
+		}
+		else if(typeof obj === "boolean"){
+			return obj.toString()
+		}
+		else if(typeof obj === "string" ){
+			return '"'+String(obj)+'"';
+		}
+		else if(typeof obj === "number"){
+			return obj.toString();
+		}
+		else if(Array.isArray(obj)){
+			function loopArray(obj){
+				var tempArray = [];
+				for(var i = 0; i < obj.length; i++){
+					var nextArray = obj[i];
+					if(Array.isArray(nextArray)){
+						tempArray.push(loopArray(nextArray));
+					}
+					else if (typeof nextArray === "string"){
+						tempArray.push('"'+nextArray+'"');
+					}
+					else {
+						tempArray.push(nextArray);
+					}
+				}	
+				return '['+tempArray+']';
+			}
+			return loopArray(obj);
+		}
+		else if(typeof obj === "object"){
+			function loopObject(obj){
+				var tempObject = [];
+				for(var key in obj){
+					var nextObject = obj[key];
+					if(typeof nextObject == "object"){
+						tempObject.push(loopObject(nextObject));
+					}
+					else if (typeof nextObject === null){
+						tempObject.push('"'+key+'"'+ ':' + String(null));
+
+					}
+					else if (typeof nextObject === "string"){
+						tempObject.push('"'+key+'"'+ ':' + '"'+nextObject+'"');
+					}
+					else {
+						tempObject.push('"'+key+'"'+ ':' + nextObject);
+					}
+				}
+				return "{"+tempObject+"}";
+			}
+			return loopObject(obj);
+		}
+
+}
